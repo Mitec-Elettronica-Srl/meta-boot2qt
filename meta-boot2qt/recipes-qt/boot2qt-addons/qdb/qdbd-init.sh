a34c0120db071fb367fc5641d6de4bd3c72f0784
Samuli Piippo <samuli.piippo@qt.io>
2025-02-17 11:19:20 +0200

    qdb: support CDC NCM protocol
    Update the QDB init script to use NCM always. NCM is supported natively
    on all hosts, so we don't need to support switching the protocol anymore.
    Change the product id, so the updated Windows driver recognize the NCM
    device correctly.
    
    Task-number: QTBUG-131690
    Change-Id: I9a4c5b2203c99c242cebe61fad74985bf9eee9fc
    Reviewed-by: Ari Parkkila <ari.parkkila@qt.io>
    (cherry picked from commit ac17e8505582c42e9fb73e10d5ad2f7e534af680)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- meta-boot2qt/recipes-qt/boot2qt-addons/qdb/qdbd-init.sh
+++ meta-boot2qt/recipes-qt/boot2qt-addons/qdb/qdbd-init.sh
@@ -3,93 +3,79 @@
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 MANUFACTURER="The Qt Company"
 QDBDAEMON=/usr/bin/qdbd
-CONFIG_GADGET_DIR_PATH=/sys/kernel/config/usb_gadget/g1
-CONFIG_NAME_AND_NUMBER="c.1"
-FUNCTIONFS_PATH=/dev/usb-ffs
+PRODUCT_STRING="Boot2Qt-USB-Ethernet-CDC/NCM"
+CONFIG_NAME="USB-Ethernet_CDC-NCM+QDB"
 
 . /etc/default/qdbd
 
-function_fs_mountpoint()
-{
-    mkdir -p $FUNCTIONFS_PATH
-    chmod 770 $FUNCTIONFS_PATH
-    mkdir -p $FUNCTIONFS_PATH/qdb
-    chmod 770 $FUNCTIONFS_PATH/qdb
-    mount -t functionfs qdb $FUNCTIONFS_PATH/qdb -o uid=0,gid=0
-}
-
 configure_gadget()
 {
-    CONFIG_GADGET_PRODUCT_STRING=$1
-    CONFIG_CONFIGURATION_NAME=$2
-    CONFIG_FUNCTION_SYMLINK=$3
-    mkdir -p $CONFIG_GADGET_DIR_PATH
-    echo $VENDOR > $CONFIG_GADGET_DIR_PATH/idVendor
-    echo $PRODUCT > $CONFIG_GADGET_DIR_PATH/idProduct
-    mkdir -p $CONFIG_GADGET_DIR_PATH/strings/0x409
-    echo $MANUFACTURER > $CONFIG_GADGET_DIR_PATH/strings/0x409/manufacturer
-    echo $CONFIG_GADGET_PRODUCT_STRING > $CONFIG_GADGET_DIR_PATH/strings/0x409/product
-    echo ${SERIAL:0:32} > $CONFIG_GADGET_DIR_PATH/strings/0x409/serialnumber
-    mkdir -p $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER/strings/0x409
-    echo $CONFIG_CONFIGURATION_NAME > $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER/strings/0x409/configuration
-    mkdir -p $CONFIG_GADGET_DIR_PATH/functions/$CONFIG_FUNCTION_SYMLINK
-    mkdir -p $CONFIG_GADGET_DIR_PATH/functions/ffs.qdb
-    ln -sf $CONFIG_GADGET_DIR_PATH/functions/$CONFIG_FUNCTION_SYMLINK $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER
-    ln -sf $CONFIG_GADGET_DIR_PATH/functions/ffs.qdb $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER
+    mkdir -p /sys/kernel/config/usb_gadget/g1
+    echo $VENDOR > /sys/kernel/config/usb_gadget/g1/idVendor
+    echo $PRODUCT > /sys/kernel/config/usb_gadget/g1/idProduct
+
+    mkdir -p /sys/kernel/config/usb_gadget/g1/strings/0x409
+    echo $MANUFACTURER > /sys/kernel/config/usb_gadget/g1/strings/0x409/manufacturer
+    echo $PRODUCT_STRING > /sys/kernel/config/usb_gadget/g1/strings/0x409/product
+    echo ${SERIAL:0:32} > /sys/kernel/config/usb_gadget/g1/strings/0x409/serialnumber
+
+    mkdir -p /sys/kernel/config/usb_gadget/g1/configs/c.1/strings/0x409
+    echo $CONFIG_NAME > /sys/kernel/config/usb_gadget/g1/configs/c.1/strings/0x409/configuration
+
+    mkdir -p /sys/kernel/config/usb_gadget/g1/functions/ncm.usb0
+    mkdir -p /sys/kernel/config/usb_gadget/g1/functions/ffs.qdb
+    ln -sf /sys/kernel/config/usb_gadget/g1/functions/ncm.usb0 /sys/kernel/config/usb_gadget/g1/configs/c.1
+    ln -sf /sys/kernel/config/usb_gadget/g1/functions/ffs.qdb /sys/kernel/config/usb_gadget/g1/configs/c.1
+
+    mkdir -p /sys/kernel/config/usb_gadget/g1/functions/ncm.usb0/os_desc/interface.ncm
+    echo WINNCM > /sys/kernel/config/usb_gadget/g1/functions/ncm.usb0/os_desc/interface.ncm/compatible_id
+    echo 000000 > /sys/kernel/config/usb_gadget/g1/functions/ncm.usb0/os_desc/interface.ncm/sub_compatible_id
+
+    mkdir -p /sys/kernel/config/usb_gadget/g1/os_desc
+    echo 1 > /sys/kernel/config/usb_gadget/g1/os_desc/use
+    echo 0x80 > /sys/kernel/config/usb_gadget/g1/os_desc/b_vendor_code
+    echo MSFT100 > /sys/kernel/config/usb_gadget/g1/os_desc/qw_sign
+    ln -sf /sys/kernel/config/usb_gadget/g1/configs/c.1 /sys/kernel/config/usb_gadget/g1/os_desc
+
+    mkdir -p /dev/usb-ffs
+    chmod 770 /dev/usb-ffs
+    mkdir -p /dev/usb-ffs/qdb
+    chmod 770 /dev/usb-ffs/qdb
+    mount -t functionfs qdb /dev/usb-ffs/qdb -o uid=0,gid=0
 }
 
 unconfigure_gadget()
 {
-    CONFIG_FUNCTION_SYMLINK=$1
-    rm $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER/$CONFIG_FUNCTION_SYMLINK
-    rm $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER/ffs.qdb
-    rmdir $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER/strings/0x409
-    rmdir $CONFIG_GADGET_DIR_PATH/configs/$CONFIG_NAME_AND_NUMBER
-    rmdir $CONFIG_GADGET_DIR_PATH/functions/$CONFIG_FUNCTION_SYMLINK
-    rmdir $CONFIG_GADGET_DIR_PATH/functions/ffs.qdb
-    rmdir $CONFIG_GADGET_DIR_PATH/strings/0x409
-    rmdir $CONFIG_GADGET_DIR_PATH
+    umount /dev/usb-ffs/qdb
+
+    rm /sys/kernel/config/usb_gadget/g1/configs/c.1/ncm.usb0
+    rm /sys/kernel/config/usb_gadget/g1/configs/c.1/ffs.qdb
+    rmdir /sys/kernel/config/usb_gadget/g1/configs/c.1/strings/0x409
+    rmdir /sys/kernel/config/usb_gadget/g1/configs/c.1
+    rmdir /sys/kernel/config/usb_gadget/g1/functions/ncm.usb0
+    rmdir /sys/kernel/config/usb_gadget/g1/functions/ffs.qdb
+    rmdir /sys/kernel/config/usb_gadget/g1/strings/0x409
+    rmdir /sys/kernel/config/usb_gadget/g1
 }
-
-if [ $USB_ETHERNET_PROTOCOL = "rndis" ]
-then
-    FUNCTION_NAME="rndis.usb0"
-    PRODUCT_STRING="Boot2Qt-USB-Ethernet-RNDIS"
-    CONFIG_NAME="USB-Ethernet_RNDIS+QDB"
-
-elif [ $USB_ETHERNET_PROTOCOL = "cdcecm" ]
-then
-    FUNCTION_NAME="ecm.usb1"
-    PRODUCT_STRING="Boot2Qt-USB-Ethernet-CDC/ECM"
-    CONFIG_NAME="USB-Ethernet_CDC-ECM+QDB"
-else
-    echo "Supported configs in /etc/default/qdbd: 'cdcecm' Mac&Linux, 'rndis' Win10&Linux. Not supported:" $USB_ETHERNET_PROTOCOL
-    exit 1
-fi
 
 if [ "$1" = "start" ]
 then
-    echo "start" $USB_ETHERNET_PROTOCOL
     b2qt-gadget-network.sh --reset
     modprobe libcomposite
     sleep 1
-    configure_gadget $PRODUCT_STRING $CONFIG_NAME $FUNCTION_NAME
-    function_fs_mountpoint
-    start-stop-daemon --start --quiet --background --exec $QDBDAEMON -- --usb-ethernet-function-name $FUNCTION_NAME
+    configure_gadget
+    start-stop-daemon --start --quiet --background --exec $QDBDAEMON -- --usb-ethernet-function-name ncm.usb0
 elif [ "$1" = "stop" ]
 then
-    echo "stop" $USB_ETHERNET_PROTOCOL
     start-stop-daemon --stop --quiet --exec $QDBDAEMON
     sleep 1
-    umount $FUNCTIONFS_PATH/qdb
-    unconfigure_gadget $FUNCTION_NAME
+    unconfigure_gadget
 elif [ "$1" = "restart" ]
 then
-    echo "restart" $USB_ETHERNET_PROTOCOL
     start-stop-daemon --stop --quiet --exec $QDBDAEMON
     b2qt-gadget-network.sh --reset
     sleep 1
-    start-stop-daemon --start --quiet --background --exec $QDBDAEMON -- --usb-ethernet-function-name $FUNCTION_NAME
+    start-stop-daemon --start --quiet --background --exec $QDBDAEMON -- --usb-ethernet-function-name ncm.usb0
 else
     echo "Usage: $0 {start|stop|restart}"
     exit 1
